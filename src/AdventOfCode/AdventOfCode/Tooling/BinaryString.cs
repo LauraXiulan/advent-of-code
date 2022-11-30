e432ee13e97aeb972f4b93a206915385957dbde8
Laura Kramer <l.kramer@tjip.com>
2022-11-30 13:03:54 +0100

    Day 03

---
+++ src/AdventOfCode/AdventOfCode/Tooling/BinaryString.cs
@@ -0,0 +1,8 @@
+﻿namespace AdventOfCode.Tooling;
+
+public static class BinaryString
+{
+    public static int SumCharArray(this string[] array, int index) => array.Sum(i => int.Parse(i[index].ToString()));
+
+    public static int ToInt(this string binary) => Convert.ToInt32(binary, 2);
+}
