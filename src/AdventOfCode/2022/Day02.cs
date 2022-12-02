0946577a0c1ce420ff1eee441c62c4f1e9f74aa4
Laura Kramer <l.kramer@tjip.com>
2022-12-02 08:14:58 +0100

    Day 02 2022

---
+++ src/AdventOfCode/2022/Day02.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode._2022;
+
+public class Day02 : Day
+{
+    public override string Example => @"A Y
+B X
+C Z";
+
+    [Test(ExpectedResult = 15)]
+    public int One_Example() => One(Example);
+
+    [Test(ExpectedResult = 14163)]
+    public override long One() => One(Input);
+
+    [Test(ExpectedResult = 12)]
+    public int Two_Example() => Two(Example);
+
+    [Test(ExpectedResult = 12091)]
+    public override long Two() => Two(Input);
+
+    private static int One(string input)
+        => input.GroupedLines()
+            .Select(s => s.Select(st => st switch
+            {
+                // A = Rock, B = Paper, C = Scissors, X = Rock (1), Y = Paper (2), Z = Scissors (3)
+                // Lost = 0, Draw = 3, Win = 6
+                "A X" => 4,
+                "A Y" => 8,
+                "A Z" => 3,
+                "B X" => 1,
+                "B Y" => 5,
+                "B Z" => 9,
+                "C X" => 7,
+                "C Y" => 2,
+                "C Z" => 6,
+                _ => 0
+            }).Sum()).ToArray().Sum();
+
+    private static int Two(string input)
+        => input.GroupedLines()
+            .Select(s => s.Select(st => st switch
+            {
+                // A = Rock (1), B = Paper (2), C = Scissors (3), X = Lose, Y = Draw, Z = Win
+                // Lost = 1, Draw = 3, Win = 5
+                "A X" => 3,
+                "A Y" => 4,
+                "A Z" => 8,
+                "B X" => 1,
+                "B Y" => 5,
+                "B Z" => 9,
+                "C X" => 2,
+                "C Y" => 6,
+                "C Z" => 7,
+                _ => 0
+            }).Sum()).ToArray().Sum();
+}
