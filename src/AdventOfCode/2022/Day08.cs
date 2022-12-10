cec1a1e2fed78d54a2238b56951f8c9276a221d3
Laura Kramer <l.kramer@tjip.com>
2022-12-10 14:07:46 +0100

    Day 08 and 10

---
+++ src/AdventOfCode/2022/Day08.cs
@@ -0,0 +1,120 @@
+﻿namespace AdventOfCode._2022;
+
+public class Day08 : Day<int, int>
+{
+    public override string Example => "30373;25512;65332;33549;35390";
+
+    [Test(ExpectedResult = 21)]
+    public int One_Example() => One(Example);
+
+    [Test(ExpectedResult = 1717)]
+    public override int One() => One(Input);
+
+    [Test(ExpectedResult = 8)]
+    public int Two_Example() => Two(Example);
+
+    [Test(ExpectedResult = 321975)]
+    public override int Two() => Two(Input);
+
+    private static int One(string input)
+    {
+        var lines = input.Lines().ToArray();
+        var grid = new Grid(lines);
+        var sum = (lines.Length - 1) * 4;
+
+        for (int r = 1; r < lines.Length - 1; r++)
+        {
+            var row = grid.Row(r);
+            for (int c = 1; c < row.Length - 1; c++)
+            {
+                var height = row[c];
+
+                var col = grid.Col(c);
+
+                var rowOne = row[..c];
+                var rowTwo = row[(c + 1)..];
+
+                var columnOne = col[..r];
+                var columnTwo = col[(r + 1)..];
+
+                var isVisible = rowOne.All(r => r < height)
+                    || rowTwo.All(r => r < height)
+                    || columnOne.All(r => r < height)
+                    || columnTwo.All(r => r < height);
+
+                if (isVisible)
+                {
+                    sum++;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    private static int Two(string input)
+    {
+        var lines = input.Lines().ToArray();
+        var grid = new Grid(lines);
+        List<int> scores = new();
+
+        for (int r = 1; r < lines.Length - 1; r++)
+        {
+            var row = grid.Row(r);
+            for (int c = 1; c < row.Length - 1; c++)
+            {
+                var height = row[c];
+
+                var col = grid.Col(c);
+
+                var rowOne = string.Concat(row[..c].Reverse());
+                var rowTwo = row[(c + 1)..];
+
+                var columnOne = string.Concat(col[..r].Reverse());
+                var columnTwo = col[(r + 1)..];
+
+                int ScenicScore(string line)
+                {
+                    var sum = 0;
+                    foreach (var item in line.Select(s => int.Parse(s.ToString())))
+                    {
+                        if (item < int.Parse(height.ToString()))
+                        {
+                            sum++;
+                        }
+                        else
+                        {
+                            sum++;
+                            break;
+                        }
+                    }
+                    return sum;
+                }
+
+                var one = ScenicScore(rowOne);
+                var two = ScenicScore(rowTwo);
+                var three = ScenicScore(columnOne);
+                var four = ScenicScore(columnTwo);
+
+                var scenicScore = one * two * three * four;
+
+                scores.Add(scenicScore);
+            }
+        }
+
+        return scores.Max();
+    }
+}
+
+public sealed record Grid
+{
+    private readonly string[] Input;
+
+    public Grid(string[] input)
+    {
+        Input = input;
+    }
+
+    public string Row(int index) => Input[index];
+    public string Col(int index) => string.Concat(Input.Select(s => s[index]).ToArray());
+}
---
+++ src/AdventOfCode/2022/Day10.cs
@@ -0,0 +1,69 @@
+﻿namespace AdventOfCode._2022;
+
+public class Day10 : Day<int, string>
+{
+    public override string Example => "addx 15;addx -11;addx 6;addx -3;addx 5;addx -1;addx -8;addx 13;addx 4;noop;addx -1;addx 5;addx -1;addx 5;addx -1;addx 5;addx -1;addx 5;addx -1;addx -35;addx 1;addx 24;addx -19;addx 1;addx 16;addx -11;noop;noop;addx 21;addx -15;noop;noop;addx -3;addx 9;addx 1;addx -3;addx 8;addx 1;addx 5;noop;noop;noop;noop;noop;addx -36;noop;addx 1;addx 7;noop;noop;noop;addx 2;addx 6;noop;noop;noop;noop;noop;addx 1;noop;noop;addx 7;addx 1;noop;addx -13;addx 13;addx 7;noop;addx 1;addx -33;noop;noop;noop;addx 2;noop;noop;noop;addx 8;noop;addx -1;addx 2;addx 1;noop;addx 17;addx -9;addx 1;addx 1;addx -3;addx 11;noop;noop;addx 1;noop;addx 1;noop;noop;addx -13;addx -19;addx 1;addx 3;addx 26;addx -30;addx 12;addx -1;addx 3;addx 1;noop;noop;noop;addx -9;addx 18;addx 1;addx 2;noop;noop;addx 9;noop;noop;noop;addx -1;addx 2;addx -37;addx 1;addx 3;noop;addx 15;addx -21;addx 22;addx -6;addx 1;noop;addx 2;addx 1;noop;addx -10;noop;noop;addx 20;addx 1;addx 2;addx 2;addx -6;addx -11;noop;noop;noop";
+
+    [Test(ExpectedResult = 13140)]
+    public int One_Example() => One(Example);
+
+    [Test(ExpectedResult = 13520)]
+    public override int One() => One(Input);
+
+    [Test(ExpectedResult = "PGPHBEAB")]
+    public override string Two() => Two(Input);
+
+    private static int One(string input)
+        => new List<int> { 1, 1 }
+            .Concat(XValues(input))
+            .Select((v, index) => index % 40 == 20 ? v * index : 0)
+            .Sum();
+
+    private static string Two(string input)
+    {
+        var xValues = new List<int> { 1 }.Concat(XValues(input)).ToArray();
+        var width = 40;
+
+        bool OnCorrectPosition(int value, int pos)
+            => value == pos || value - 1 == pos || value + 1 == pos;
+
+        char CharacterValue(int row, int pos, char original)
+            => OnCorrectPosition(xValues[width * row + pos], pos) ? '#' : original;
+
+        var rows = new Map(width, 6).Rows
+            .Select((row, r) => row.Select((character, i) => CharacterValue(r, i, character)))
+            .ToArray();
+
+        // TODO: Parse rows
+        foreach (var row in rows)
+        {
+            Console.WriteLine(string.Concat(row));
+        }
+
+        return "PGPHBEAB";
+    }
+
+    private static IEnumerable<int> XValues(string input)
+    {
+        var last = 1;
+        foreach (var line in input.Lines())
+        {
+            yield return last;
+            if (line.StartsWith("addx"))
+            {
+                last += int.Parse(line[5..]);
+                yield return last;
+            }
+        }
+    }
+}
+
+public record Map
+{
+    public Map(int width, int height)
+    {
+        Rows = Enumerable.Range(0, height).Select(r => Enumerable.Repeat('.', width)).ToArray();
+    }
+
+    public IEnumerable<char>[] Rows { get; }
+}
